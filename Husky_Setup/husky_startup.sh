fe567984ebaeb3d28818ff4ff76f46a049bde0eb
Joshua Ra'anan <jraanan@andrew.cmu.edu>
2019-06-29 13:49:08 -0400

    Setup husky_startup.sh for multi-computer networking

--- Husky_Setup/husky_startup.sh
+++ Husky_Setup/husky_startup.sh
@@ -5,6 +5,20 @@ source /home/nvidia/SureClean_UGV/devel/setup.bash
 sleep 5
 # Adjust the imu port latency
 setserial /dev/ttyIMU0 low_latency
+# Check Wifi network name, to set ROS_MASTER_URI
+NETWORK_NAME=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2)
+if [ $NETWORK_NAME == "CMU-DEVICE" ]
+then
+  # set hostname on system, which sets ROS_HOSTNAME on husky-core restart
+  hostname husky-cmu
+  # Set master uri
+  export ROS_MASTER_URI=http://husky-cmu:11311
+else
+  # set hostname on system, which sets ROS_HOSTNAME on husky-core restart
+  hostname husky-tp
+  # Set master uri
+  export ROS_MASTER_URI=http://husky-tp:11311
+fi
 # Restart husky service to launch default husky nodes and roscore
 sudo service husky-core restart
 # wait until roscore is running
